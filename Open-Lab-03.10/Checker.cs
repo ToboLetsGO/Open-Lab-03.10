eb3d7c78c439a253bf0acb30ae5b82307b6c8816
ToboLestGoIT <tobo1369@gmail.com>
2021-11-09 12:51:56 +0100

    Hotovo

--- Open-Lab-03.10/Checker.cs
+++ Open-Lab-03.10/Checker.cs
@@ -6,7 +6,15 @@ namespace Open_Lab_03._10
     {
         public int GetNumberOfCharsInString(char letter, string str)
         {
-            throw new NotImplementedException();
+            int number_of_chars = 0;
+            foreach (var item in str)
+            {
+                if (item == letter)
+                {
+                    number_of_chars++;
+                }
+            }
+            return number_of_chars;
         }
     }
 }
